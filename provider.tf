e2274604a607c061159daa25769cd70d817c9fe4
Realfemi <108326366+Realfemi@users.noreply.github.com>
2022-07-20 18:20:59 +0100

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.53"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.53"
+    }
+    
+  }
+}
+
+provider "google" {
+  project = var.project
+}
+
+provider "google-beta" {
+  project = var.project
+}
