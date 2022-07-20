58f67edf6b9b4de2cc360c7426c1646fb5020ac9
Realfemi <108326366+Realfemi@users.noreply.github.com>
2022-07-20 18:23:56 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,79 @@
+
+variable "domain_name" {
+ 
+  type = string
+}
+
+variable "ssl_cert" {
+ 
+  type = string
+}
+
+variable "instance_template" {
+ 
+  type = string
+}
+
+variable "vpc_network" {
+ 
+  type = string
+}
+
+variable "vpc_subnet" {
+ 
+  type = string
+}
+variable "region" {
+ 
+  type = string
+}
+
+variable "zone" {
+ 
+  type = string
+}
+
+variable "lb_ip_address" {
+ 
+  type = string
+}
+
+variable "mig_name" {
+ 
+  type = string
+}
+
+variable "firewall_rule" {
+ 
+  type = string
+}
+
+variable "health_check" {
+ 
+  type = string
+}
+
+variable "backend_service" {
+ 
+  type = string
+}
+
+variable "url_map" {
+ 
+  type = string
+}
+
+variable "proxy_name" {
+ 
+  type = string
+}
+
+variable "forwarding_rule" {
+ 
+  type = string
+}
+
+variable "project" {
+ 
+  type = string
+}
